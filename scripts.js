06aa4c2f2c57c2be73f7cbe5cda56ad94d3cc49f
karim-fayed <101948015+karim-fayed@users.noreply.github.com>
2024-04-18 16:39:34 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const amountInput = document.getElementById("amount");
     const percentageInput = document.getElementById("percentage");
     const numberInput = document.getElementById("numberInput");
-    const convertButton = document.getElementById("convertButton");
 
     document.getElementById("percentageForm").addEventListener("submit", function(event) {
         event.preventDefault();
@@ -26,36 +25,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    convertButton.addEventListener("click", function() {
+    document.getElementById("convertButton").addEventListener("click", function() {
         convertToArabic();
     });
 
-    async function convertToArabic() {
+    function convertToArabic() {
         let numberValue = numberInput.value.trim();
         if (numberValue !== '') {
-            const encodedParams = new URLSearchParams();
-            encodedParams.set('the_number', numberValue);
-            encodedParams.set('unit', ' ريال سعودي');
-            encodedParams.set('hundreds_form', 'مائة');
-
-            const options = {
-                method: 'POST',
-                url: 'https://tafqit.p.rapidapi.com/convert',
-                headers: {
-                    'content-type': 'application/x-www-form-urlencoded',
-                    'X-RapidAPI-Key': '15c80c7f58msh0a3d1ac99e47bccp1959c7jsn690a59131f90',
-                    'X-RapidAPI-Host': 'tafqit.p.rapidapi.com'
-                },
-                data: encodedParams,
-            };
-
-            try {
-                const response = await axios.request(options);
-                document.getElementById("convertedResult").innerHTML = "<h3>نتيجة التفقيط:</h3><p>" + response.data.tafqit + "</p>";
-            } catch (error) {
-                console.error(error);
-            }
-
+            let convertedNumber = numberToArabic(numberValue);
+            document.getElementById("convertedResult").innerHTML = "<h3>نتيجة التفقيط:</h3><p>" + convertedNumber + "</p>";
         } else {
             alert("الرجاء إدخال الرقم للتفقيط.");
         }
