1ca2a0cf1f88b27b40a3ad39b9452819c9fdcfb7
karim-fayed <101948015+karim-fayed@users.noreply.github.com>
2024-04-18 15:58:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// JavaScript
-document.getElementById("percentageForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    let amount = parseFloat(document.getElementById("amount").value);
-    let percentage = parseFloat(document.getElementById("percentage").value);
-    let operation = document.getElementById("operation").value;
-    let result = 0;
+// JavaScript code
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("percentageForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        let amount = parseFloat(document.getElementById("amount").value);
+        let percentage = parseFloat(document.getElementById("percentage").value);
+        let operation = document.getElementById("operation").value;
+        let result = 0;
 
-    if (operation === "extract") {
-        result = (amount * percentage) / 100;
-    } else if (operation === "subtract") {
-        result = amount - ((amount * percentage) / 100);
-    } else if (operation === "add") {
-        result = amount + ((amount * percentage) / 100);
-    }
+        if (operation === "extract") {
+            result = (amount * percentage) / 100;
+        } else if (operation === "subtract") {
+            result = amount - ((amount * percentage) / 100);
+        } else if (operation === "add") {
+            result = amount + ((amount * percentage) / 100);
+        }
 
-    document.getElementById("result").innerHTML = "<h3>النتيجة:</h3><p>" + result.toFixed(2) + "</p>";
+        document.getElementById("result").innerHTML = "<h3>النتيجة:</h3><p>" + result.toFixed(2) + "</p>";
+    });
 });
